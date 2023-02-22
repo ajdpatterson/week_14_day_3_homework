7f448731b123ea6a0e027beac9d4ddecde000178
student <student@students-MacBook-Pro.local>
2023-02-22 19:59:52 +0000

    homework

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -0,0 +1,36 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class DistilleryController {
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/distilleries")
+    public ResponseEntity<List<Distillery>> getAllDistilleries(Optional<String> region, Optional<Integer> year){
+        if ((region.isPresent()) ) {
+            return new ResponseEntity<>(distilleryRepository.findByRegion(region), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/distilleries/age")
+    public ResponseEntity<List<Distillery>> getAllDistilleriesAge(Integer age){
+        return new ResponseEntity<>(distilleryRepository.findByWhiskiesAge(age), HttpStatus.OK);
+    }
+
+
+
+
+}
